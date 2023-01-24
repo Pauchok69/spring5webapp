8cefcee05617351ffc6b1e3a2851d9657a2307d1
Andy <andrew.zerko@gmail.com>
2023-01-24 20:45:41 +0200

    Add OneToMany relation between Publisher and Book.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -24,28 +24,34 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher times = new Publisher("Times", "1st street", "New-York", "State", "12333");
+        publisherRepository.save(times);
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        ddd.setPublisher(times);
+        times.getBooks().add(ddd);
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(times);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEjb = new Book("J2EE Development without EJB", "93939383");
         rod.getBooks().add(noEjb);
         noEjb.getAuthors().add(rod);
+        noEjb.setPublisher(times);
+        times.getBooks().add(noEjb);
 
         authorRepository.save(rod);
         bookRepository.save(noEjb);
-
-        Publisher times = new Publisher("Times", "1st street", "New-York", "State", "12333");
-
         publisherRepository.save(times);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Numbers of books: " + bookRepository.count());
         System.out.println("publisherRepository.count() = " + publisherRepository.count());
+        System.out.println("times.getBooks().size() = " + times.getBooks().size());
     }
 }
